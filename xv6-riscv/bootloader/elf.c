c8a88b452636f0893f836a398b9ca05d37ef56e0
Arnav Aghav <arnav.aghav@gmail.com>
2023-09-09 18:39:27 -0700

    All soln except kernelpmp2

--- xv6-riscv/bootloader/elf.c
+++ xv6-riscv/bootloader/elf.c
@@ -12,21 +12,23 @@ struct elfhdr* kernel_elfhdr;
 struct proghdr* kernel_phdr;
 
 
-// Version 1
 uint64 find_kernel_load_addr(enum kernel ktype) {
     /* CSE 536: Get kernel load address from headers */
-    kernel_elfhdr = (struct elfhdr*) RAMDISK;
+    
+    uint64 base_addr = (ktype == NORMAL) ? RAMDISK : RECOVERYDISK;
+    kernel_elfhdr = (struct elfhdr*) base_addr;
 
     // Step 1: Check that the ELF header is valid
     if (kernel_elfhdr->magic != ELF_MAGIC) {
         panic("Invalid ELF file");
     }
+    
     // Step 2: Grab the offset and size of the program headers from the ELF header
     uint64 phoff = kernel_elfhdr->phoff;
     uint64 phsize = kernel_elfhdr->phentsize;
 
     // Step 3: Navigate to the .text section's header in the program headers
-    kernel_phdr = (struct proghdr*) (RAMDISK + phoff + phsize);
+    kernel_phdr = (struct proghdr*) (base_addr + phoff + phsize);
 
     // Step 4: Retrieve the starting address of the .text section from the program header
     uint64 kernload_start = kernel_phdr->vaddr;
@@ -34,47 +36,22 @@ uint64 find_kernel_load_addr(enum kernel ktype) {
     return kernload_start;
 }
 
-uint64 find_kernel_size(enum kernel ktype) {
-    /* CSE 536: Get kernel binary size from headers */
-    
-    // Version 2
-    struct elfhdr* kernel_elfhdr = (struct elfhdr*) RAMDISK;
-
-    // Step 2: Find the offset to the section header table and the number of entries
-    uint64 shoff = kernel_elfhdr->shoff;
-    uint16 shnum = kernel_elfhdr->shnum;
-    uint16 shentsize = kernel_elfhdr->shentsize;
-
-    // Step 3: Calculate the size of the binary as the offset of the last section header plus its size
-    uint64 kernel_size = shoff + (shnum * shentsize);
-
-    return kernel_size;
-
-}
-
 uint64 find_kernel_entry_addr(enum kernel ktype) {
     /* CSE 536: Get kernel entry point from headers */
-    struct elfhdr* kernel_elfhdr = (struct elfhdr*) RAMDISK;
+    
+    uint64 base_addr = (ktype == NORMAL) ? RAMDISK : RECOVERYDISK;
+    struct elfhdr* kernel_elfhdr = (struct elfhdr*) base_addr;
+    
     uint64 entry_addr = kernel_elfhdr->entry;
     return entry_addr;
 }
 
-// uint64 find_kernel_load_addr(enum kernel type) {
-//     kernel_elfhdr = (struct elfhdr*) RAMDISK;
-//     uint64 elfhdr_phoff = kernel_elfhdr->phoff;	
-//     uint64 elfhdr_psize = kernel_elfhdr->phentsize;
-//     kernel_phdr = (struct proghdr*) (RAMDISK + elfhdr_phoff + elfhdr_psize);
-//     return kernel_phdr->vaddr;
-// }
-
-// uint64 find_kernel_size(enum kernel type) {
-//     kernel_elfhdr = (struct elfhdr*)RAMDISK;
-//     int total_size = kernel_elfhdr->shoff + (kernel_elfhdr->shnum * kernel_elfhdr->shentsize);
-//     return total_size;
-// }
-
-// uint64 find_kernel_entry_addr(enum kernel type) {
-//     kernel_elfhdr = (struct elfhdr*)RAMDISK;
-//     return kernel_elfhdr->entry;
-// }
-
+uint64 find_kernel_size(enum kernel ktype) {
+    /* CSE 536: Get kernel size from headers */
+    
+    uint64 base_addr = (ktype == NORMAL) ? RAMDISK : RECOVERYDISK;
+    struct elfhdr* kernel_elfhdr = (struct elfhdr*) base_addr;
+    
+    int total_size = kernel_elfhdr->shoff + (kernel_elfhdr->shnum * kernel_elfhdr->shentsize);
+    return total_size;
+}
--- xv6-riscv/bootloader/start.c
+++ xv6-riscv/bootloader/start.c
@@ -43,58 +43,44 @@ void panic(char *s)
 /* CSE 536: Boot into the RECOVERY kernel instead of NORMAL kernel
  * when hash verification fails. */
 void setup_recovery_kernel(void) {
-  uint64 recovery_kernel_load_addr       = find_kernel_load_addr(RECOVERYDISK);
-  uint64 recovery_kernel_binary_size     = find_kernel_size(RECOVERYDISK);     
-  uint64 recovery_kernel_entry           = find_kernel_entry_addr(RECOVERYDISK);
 
-  struct buf recovery_kernel_buf;
+  uint64 kernel_load_addr       = find_kernel_load_addr(RECOVERY);
+  uint64 kernel_binary_size     = find_kernel_size(RECOVERY);     
+  uint64 kernel_entry           = find_kernel_entry_addr(RECOVERY);
+
+  // Create a buffer to hold the data
+  struct buf kernel_buf;
 
   // Calculate the number of blocks to copy (excluding the first 4KB)
-  uint64 num_blocks = (recovery_kernel_binary_size - 0x1000) / BSIZE;
-
-  // Copy the recovery kernel binary excluding the first 4KB (which contains the ELF header)
+  uint64 num_blocks = (kernel_binary_size - 0x1000) / BSIZE;
+  
+  // Copy the kernel binary excluding the first 4KB 
   for (uint64 i = 0; i < num_blocks; i++) {
-      recovery_kernel_buf.blockno = (0x1000 / BSIZE) + i;
-      kernel_copy(RECOVERYDISK, &recovery_kernel_buf);
-      memmove((void *)(recovery_kernel_load_addr + i * BSIZE), recovery_kernel_buf.data, BSIZE);
+    kernel_buf.blockno = (0x1000 / BSIZE) + i;
+    kernel_copy(RECOVERY, &kernel_buf);
+    memmove((void *)(kernel_load_addr + i * BSIZE), kernel_buf.data, BSIZE);
   }
 
-  w_mepc((uint64) recovery_kernel_entry);
+  w_mepc((uint64) kernel_entry);
 
 }
 
 /* CSE 536: Function verifies if NORMAL kernel is expected or tampered. */
 bool is_secure_boot(void) {
   bool verification = true;
-
-  /* Read the binary and update the observed measurement 
-   * (simplified template provided below) */
   
-  /*
-  
-  // Initialize context
-  sha256_init(&sha256_ctx);
-  
-  // Setup sturctures
-  struct buf b;
-  uint64 kernel_binary_size = find_kernel_size(NORMAL);
-  uint64 num_blocks = kernel_binary_size / BSIZE;
+  sha256_init(&sha256_ctx); // Initialize context
+  uint64 kernel_binary_size = find_kernel_size(NORMAL); 
 
-  // Copy each block and update the hash
-  for (uint64 i = 0; i < num_blocks; i++) {
-    b.blockno = i;
-    kernel_copy(NORMAL, &b);
-    sha256_update(&sha256_ctx, (const unsigned char*) b.data, BSIZE);
-  }
-
-  // sha256_update(&sha256_ctx, (const unsigned char*) b.data, BSIZE);
-    
+  sha256_update(&sha256_ctx, (const BYTE*) RAMDISK, kernel_binary_size);
   sha256_final(&sha256_ctx, sys_info_ptr->observed_kernel_measurement);
 
-  //  1. Compare observed measurement with expected hash
-  // verification = memcmp(sys_info_ptr->observed_kernel_measurement, sys_info_ptr->expected_kernel_measurement, 32) == 0;
   for (int i = 0; i < 32; i++) {
-    if (sys_info_ptr->observed_kernel_measurement[i] != sys_info_ptr->expected_kernel_measurement[i]) {
+    sys_info_ptr->expected_kernel_measurement[i] = trusted_kernel_hash[i];
+  }
+
+  for (int i = 0; i < 32; i++) {
+    if (sys_info_ptr->observed_kernel_measurement[i] != trusted_kernel_hash[i]) {
       verification = false;
       break;
     }
@@ -104,16 +90,6 @@ bool is_secure_boot(void) {
   if (!verification)
     setup_recovery_kernel();
   
-  //  3. Copy expected kernel hash to the system information table 
-  else {
-    // memcpy(sys_info_ptr->expected_kernel_measurement, sys_info_ptr->observed_kernel_measurement, 32);
-    for (int i = 0; i < 32; i++) {
-      sys_info_ptr->expected_kernel_measurement[i] = sys_info_ptr->observed_kernel_measurement[i];
-    }  
-  }
-
-  */
-
   return verification;
 }
 
@@ -124,7 +100,7 @@ void start()
   // sys_info_ptr = (struct sys_info*) 0x80080000;
   sys_info_ptr = SYSINFOADDR;
   sys_info_ptr->bl_start = 0x80000000;
-  sys_info_ptr->bl_end = (uint64)&end;
+  sys_info_ptr->bl_end = (uint64)end;
   sys_info_ptr->dr_start = 0x80000000;
   sys_info_ptr->dr_end = PHYSTOP;
 
@@ -150,17 +126,31 @@ void start()
 
   /* CSE 536: With kernelpmp1, isolate upper 10MBs using TOR */ 
   #if defined(KERNELPMP1)
-    // Verion 1
-    // w_pmpaddr0((PHYSTOP - (10 * 1024 * 1024)) >> 2);
-    // w_pmpcfg0(0x18 | (1 << 7)); 
-    w_pmpcfg0(0b00001111); // R, W, X bits set and A field set to TOR (b000)
-    w_pmpaddr0((117 * 1024 * 1024) >> 2); // The address is right-shifted by 2 to fit into the register correctly
+    w_pmpaddr0(((((117 * 1024 * 1024) +0x80000000) >> 2))); 
+    w_pmpcfg0(0b00001111); 
   #endif
 
-  /* CSE 536: With kernelpmp2, isolate 118-120 MB and 122-126 MB using NAPOT */ 
+  /* CSE 536: With kernelpmp2, isolate 
+  
+  1 8000 - 118 TOR
+  2 118 - 120
+  3 120 - 122 NAPOT
+  4 122 - 126
+  5 126 - 128 NAPOT
+  
+  118-120 MB and 122-126 MB using NAPOT */ 
   #if defined(KERNELPMP2)
-    w_pmpaddr0(((118 * 1024 * 1024) >> 2) | ((1 << 3) - 1)); // Set NAPOT range
-    w_pmpcfg0(0x18 | (1 << 7));
+
+
+    w_pmpcfg0(0b000111100001111000011110000111100001111); 
+
+    // Setting up PMP region to isolate 122-126 MB (4MB)
+    w_pmpaddr0(((((118 * 1024 * 1024) + 0x80000000) >> 2))); 
+    w_pmpaddr1(((((120 * 1024 * 1024) + 0x80000000) >> 2)) | 0x1FFFFF); 
+    w_pmpaddr2(((((122 * 1024 * 1024) + 0x80000000) >> 2))); 
+    w_pmpaddr3(((((126 * 1024 * 1024) + 0x80000000) >> 2)) | 0x7FFFFF); 
+    w_pmpaddr4(((((126 * 1024 * 1024) + 0x80000000) >> 2))); 
+
   #endif
 
   /* CSE 536: Verify if the kernel is untampered for secure boot */
@@ -171,9 +161,6 @@ void start()
   }
   
   /* CSE 536: Load the NORMAL kernel binary (assuming secure boot passed). */
-  // uint64 kernload_start         = find_kernel_load_addr(NORMAL);
-  
-  // Version 1
   /* CSE 536: Write the correct kernel entry point */
   uint64 kernel_load_addr       = find_kernel_load_addr(NORMAL);
   uint64 kernel_binary_size     = find_kernel_size(NORMAL);     
@@ -196,10 +183,6 @@ void start()
   w_mepc((uint64) kernel_entry);
  
  out:
-  /* CSE 536: Provide system information to the kernel. */
-
-  /* CSE 536: Send the observed hash value to the kernel (using sys_info_ptr) */
-
   // delegate all interrupts and exceptions to supervisor mode.
   w_medeleg(0xffff);
   w_mideleg(0xffff);
